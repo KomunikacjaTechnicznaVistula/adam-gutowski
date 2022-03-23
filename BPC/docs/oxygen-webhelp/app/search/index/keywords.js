0eb989a872a8ec4d1df2cbbe3e0dc2fc28a0710c
adgut1509 <adgut1509@gmail.com>
2022-03-23 21:07:45 +0100

    new

---
+++ BPC/docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+define(function() {var keywords=[{w:"Cz\u0119stochowa",p:["p0"]},{w:"Map",p:["p1"]},{w:"Katowice",p:["p2"]},{w:"Bia\u0142ystok",p:["p3"]},{w:"Warsaw",p:["p4"]},{w:"\u0141\u00F3d\u017A",p:["p5"]},{w:"Krak\u00F3w",p:["p6"]},{w:"Wroc\u0142aw",p:["p7"]},{w:"Pozna\u0144",p:["p8"]},{w:"Gda\u0144sk",p:["p9"]},{w:"Szczecin",p:["p10"]},{w:"Bydgoszcz",p:["p11"]},{w:"Lublin",p:["p12"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2];
+ph["p3"]=[3];
+ph["p4"]=[4];
+ph["p5"]=[5];
+ph["p6"]=[6];
+ph["p7"]=[7];
+ph["p8"]=[8];
+ph["p9"]=[9];
+ph["p10"]=[10];
+ph["p12"]=[12];
+ph["p11"]=[11];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
